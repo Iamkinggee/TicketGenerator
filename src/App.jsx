465c8ca833c43fb4f9129c97da62d113b15a5459
Iamkinggee <iamgodsentt@gmail.com>
2025-02-11 20:07:24 +0100

    update

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import SelectTicket from './pages/SelectTicket'
+
+
+function App() {
+
+
+  return (
+    <>
+    <SelectTicket/>
+
+    
+    
+    </>
+   
+      
+  )
+}
+
+export default App
---
+++ src/pages/Tickpage2.jsx
@@ -0,0 +1,9 @@
+// import React from 'react'
+
+// function Tickpage2() {
+//   return (
+//     <div>Tickpage2</div>
+//   )
+// }
+
+// export default Tickpage2
