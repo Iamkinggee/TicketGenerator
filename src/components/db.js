93ec24c08e23c1556eb98956a40ca7eee002cc95
Godsent Oko-Ose <111008769+Iamkinggee@users.noreply.github.com>
2025-02-15 00:39:20 +0100

    Update db.js
    Deleted something

--- src/components/db.js
+++ src/components/db.js
@@ -1,37 +1,3 @@
-// // db.js
-// import { openDB } from 'idb';
-
-// const DB_NAME = 'formDB';
-// const STORE_NAME = 'formStore';
-
-// export const initDB = async () => {
-//   return openDB(DB_NAME, 1, {
-//     upgrade(db) {
-//       if (!db.objectStoreNames.contains(STORE_NAME)) {
-//         db.createObjectStore(STORE_NAME, { keyPath: 'id', autoIncrement: true });
-//       }
-//     },
-//   });
-// };
-
-// export const addFormData = async (data) => {
-//   const db = await initDB();
-//   const tx = db.transaction(STORE_NAME, 'readwrite');
-//   const store = tx.objectStore(STORE_NAME);
-//   await store.add(data);
-//   await tx.done;
-// };
-
-// export const getFormData = async () => {
-//   const db = await initDB();
-//   const tx = db.transaction(STORE_NAME, 'readonly');
-//   const store = tx.objectStore(STORE_NAME);
-//   const allData = await store.getAll();
-//   return allData[allData.length - 1]; 
-// };
-
-
-
 
 // db.js
 import { openDB } from 'idb';
@@ -62,7 +28,7 @@ export const getFormData = async () => {
   const tx = db.transaction(STORE_NAME, 'readonly');
   const store = tx.objectStore(STORE_NAME);
   const allData = await store.getAll();
-  return allData[allData.length - 1]; // Get the latest saved form data
+  return allData[allData.length - 1]; 
 };
 
 // Cloudinary Upload Utility
